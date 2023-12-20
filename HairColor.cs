cf530e799ddee6e7fac3babd79b3375de9e2863a
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-20 11:23:19 +0700

    Добавьте файлы проекта.

---
+++ HairColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectGraph
+{
+    internal enum HairColor{
+        Auburn, Black, Blonde, Blue, Brown, Gray, Platinum, Purple, Red, White
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ObjectGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guys = new List<Guy>
+            {
+                new Guy()
+                {
+                    Name = "Bob",
+                    Clothes = new Outfit() { Top = "t-shirt", Bottom = "jeans" },
+                    Hair = new HairStyle() { Color = HairColor.Red, Length = 3.5f }
+                },
+                new Guy()
+                {
+                    Name = "Joe",
+                    Clothes = new Outfit() { Top = "polo", Bottom = "slacks" },
+                    Hair = new HairStyle() { Color = HairColor.Gray, Length = 2.7f }
+                },
+            };
+            var jsonString = JsonSerializer.Serialize(guys);
+            Console.WriteLine(jsonString);
+            var copyOfGuys = JsonSerializer.Deserialize<List<Guy>>(jsonString);
+            foreach (var guy in copyOfGuys)
+                Console.WriteLine("I deserialized this guy: {0}", guy);
+            Console.WriteLine("Press any key to exit");
+            char key = Console.ReadKey().KeyChar;
+        }
+    }
+}
